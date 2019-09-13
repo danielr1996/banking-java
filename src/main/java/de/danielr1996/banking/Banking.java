87fa2799a6ebf8b5ec2ea4f7aefb57d2de3ba188
RichterD042 <Daniel.Richter@arbeitsagentur.de>
2019-09-13 14:08:17 +0200

    feat: okta

--- src/main/java/de/danielr1996/banking/Banking.java
+++ src/main/java/de/danielr1996/banking/Banking.java
@@ -2,21 +2,36 @@ package de.danielr1996.banking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 import java.security.KeyStore;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableResourceServer
-@EnableOAuth2Sso
 public class Banking {
   public static void main(String[] args) {
 //    KeyStore.Entry newEntry = new KeyStore.TrustedCertificateEntry();
 
     SpringApplication.run(Banking.class, args);
+  }
+
+  @Configuration
+  static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+//        .authorizeRequests()
+//        .anyRequest()
+//        .authenticated()
+//        .and()
+        .oauth2ResourceServer().jwt();
+    }
   }
 }

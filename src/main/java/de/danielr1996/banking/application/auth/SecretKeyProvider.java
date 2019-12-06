7731cd296ceb39fc8410562cdbf506f7aa8c1b6f
Daniel Richter <danielrichter@posteo.de>
2019-12-06 18:59:01 +0100

    feat(auth): Use signing key from classpath

--- src/main/java/de/danielr1996/banking/application/auth/SecretKeyProvider.java
+++ src/main/java/de/danielr1996/banking/application/auth/SecretKeyProvider.java
@@ -2,19 +2,29 @@ package de.danielr1996.banking.application.auth;
 
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.*;
 import java.security.Key;
 
 @Configuration
+@Slf4j
 public class SecretKeyProvider {
   public static final String SIGN_KEY_TOKEN = "SIGN_KEY_TOKEN";
 
+  // FIXME: Load key from file
   @Bean
   @Qualifier(SIGN_KEY_TOKEN)
   public Key getSigningKey() {
-    return Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    try {
+      ObjectInputStream ois = new ObjectInputStream(SecretKeyProvider.class.getClassLoader().getResourceAsStream("key.ser"));
+      return (Key) ois.readObject();
+    } catch (IOException | ClassNotFoundException e) {
+      log.error("Cannot deserialize signing key, generating new key");
+      return Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
   }
 }

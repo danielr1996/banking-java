5c590429efc1ad44b3905a81fba42d7d6ac15783
Daniel Richter <danielrichter@posteo.de>
2019-12-23 02:57:23 +0100

    test

--- .github/actions/kubectl-deploy-action/entrypoint.sh
+++ .github/actions/kubectl-deploy-action/entrypoint.sh
@@ -4,12 +4,8 @@ set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo $INPUT_KUBECONFIG | base64 -d > /tmp/config
-cat /tmp/config
-
-export KUBECONFIG=/tmp/config
 
 /opt/kubectl kustomize deployment/overlays/replace > template.yaml
 envsubst '$VERSION $INSTANCE' < template.yaml > deployment.yaml
 cat deployment.yaml
-/opt/kubectl config view
-/opt/kubectl apply -f deployment.yaml
+/opt/kubectl --kubeconfig /tmp/config apply -f deployment.yaml

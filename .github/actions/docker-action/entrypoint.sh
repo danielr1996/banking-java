61947421308f8962f1a530844c01de6034ca941b
Daniel Richter <danielrichter@posteo.de>
2019-12-23 03:30:55 +0100

    test
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- .github/actions/docker-action/entrypoint.sh
+++ .github/actions/docker-action/entrypoint.sh
@@ -4,17 +4,14 @@ echo ########################
 echo # Github Docker Action #
 echo ########################
 echo ImageName: $INPUT_IMAGENAME
-echo ImageTag: $INPUT_IMAGETAG
-#IMAGETAG_SLUG=$(echo $INPUT_IMAGETAG | tr / -)
-#echo ImageTagSlug: $IMAGETAG_SLUG
-echo DockerUser: $INPUT_DOCKERUSER
-echo DockerPasword: $INPUT_DOCKERPASSWORD
+echo ImageTags: $INPUT_IMAGETAGS
 
 # Login
 echo $INPUT_DOCKERPASSWORD | docker login --username $INPUT_DOCKERUSER --password-stdin
 
 # Build
-docker build -t $INPUT_IMAGENAME:$INPUT_IMAGETAG .
+
+#docker build -t $INPUT_IMAGENAME:$INPUT_IMAGETAG .
 
 # Push
-docker push $INPUT_IMAGENAME:$INPUT_IMAGETAG
+#docker push $INPUT_IMAGENAME:$INPUT_IMAGETAG

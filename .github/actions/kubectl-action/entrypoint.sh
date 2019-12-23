04a7bf6db0f0bec9794064cfad15f41ca3700372
Daniel Richter <danielrichter@posteo.de>
2019-12-23 01:06:13 +0100

    test

--- .github/actions/kubectl-action/entrypoint.sh
+++ .github/actions/kubectl-action/entrypoint.sh
@@ -5,7 +5,5 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
-pwd
-ls
-cat deployment.yaml
-/bin/sh -c "/opt/kubectl $*"
+
+echo "##[set-output name=kubectl;]$(/opt/kubectl $*)"

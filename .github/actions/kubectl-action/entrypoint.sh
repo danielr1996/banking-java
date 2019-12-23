919f38f2d0d717defff9d109e2d716435d22f6dc
Daniel Richter <danielrichter@posteo.de>
2019-12-23 01:12:56 +0100

    test
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- .github/actions/kubectl-action/entrypoint.sh
+++ .github/actions/kubectl-action/entrypoint.sh
@@ -6,4 +6,4 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 
-echo "##[set-output name=kubectl;]$(/opt/kubectl $*)"
+echo "##[set-output name=kubectl;]$(/opt/kubectl $* | base64)"
--- .github/actions/kubectl-action/entrypoint2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-set -e
-echo "##[set-output name=kubectl;]$(kubectl $*)"

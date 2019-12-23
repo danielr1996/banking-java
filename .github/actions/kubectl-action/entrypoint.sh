cb06d91179aee7b41d775689842cb2477b013a8b
Daniel Richter <danielrichter@posteo.de>
2019-12-23 01:32:01 +0100

    test
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- .github/actions/kubectl-action/entrypoint.sh
+++ .github/actions/kubectl-action/entrypoint.sh
@@ -6,4 +6,5 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
 
-echo "##[set-output name=kubectl;]$(/opt/kubectl $* | base64 -w 0)"
+# Base64 encode and remove new lines (busybox/alpine does not support base64 -w0)
+echo "##[set-output name=kubectl;]$(/opt/kubectl $* | base64 | sed ':a;N;$!ba;s/\n//g')"
